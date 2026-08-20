a956b6701cd7ba17270552d320286021105c835c
Ahmed Attafi <121392260+Attafii@users.noreply.github.com>
2026-08-20 18:26:10 +0100

    fix: remove invalid generateId from auth advanced options

--- lib/auth.ts
+++ lib/auth.ts
@@ -118,14 +118,6 @@ function createAuth() {
             path: "/"
           }
         }
-      },
-      generateId: () => {
-        const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        let id = "";
-        for (let i = 0; i < 32; i++) {
-          id += chars.charAt(Math.floor(Math.random() * chars.length));
-        }
-        return id;
       }
     },
     secret: process.env.BETTER_AUTH_SECRET
